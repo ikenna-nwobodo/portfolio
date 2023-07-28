e3d086dd7c0ff8b9c28f4d5fc2b00d109d25a2f9
Nwobodo Ikenna <nikenna06@gmail.com>
2023-07-28 16:44:13 +0100

    bgchange

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
 import Renders from "./Renders";
-
+import vid from "./images/bg/bggrad.mp4";
 function App() {
   return (
-    <div className="main">
-      <Renders />
+    <div>
+      <video autoPlay loop muted>
+        <source src={vid} type="video/mp4" />
+      </video>
+      <div className="main">
+        <Renders />
+      </div>
     </div>
   );
 }
